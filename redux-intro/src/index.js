40fd8a85274baec7d0bfdf49dcfde42583b343c8
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-04 15:59:44 +0300

    fixed

--- redux-intro/src/index.js
+++ redux-intro/src/index.js
@@ -7,6 +7,7 @@ import configureStore from "./redux/reducers/configureStore";
 import { Provider } from "react-redux";
 
 const store = configureStore();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
