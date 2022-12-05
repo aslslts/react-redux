0d0855ce65b5ab5ba63a6e597e7d97a197d30934
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-06 00:47:02 +0300

    redux- thunk

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { bindActionCreators } from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions";
 class CategoryList extends Component {
+  componentDidMount() {
+    this.props.actions.getCategories();
+  }
   render() {
     return (
       <div>
         <h3>Categories</h3>
+        <ListGroup>
+          {this.props.categories.map((category) => (
+            <ListGroupItem key={category.id}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
         <h5>Seçili Kategori : {this.props.currentCategory.categoryName}</h5>
       </div>
     );
@@ -13,7 +26,18 @@ class CategoryList extends Component {
 function mapStateToProps(state) {
   return {
     currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      ),
+    },
   };
 }
 
-export default connect(mapStateToProps)(CategoryList);
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
--- northwind-redux/src/redux/actions/categoryActions.js
+++ northwind-redux/src/redux/actions/categoryActions.js
@@ -3,3 +3,20 @@ import * as actionTypes from "./actionTypes";
 export function changeCategory(category) {
   return { type: actionTypes.CHANGE_CATEGORY, payload: category };
 }
+
+export function getCategoriesSuccess(categories) {
+  return {
+    type: actionTypes.GET_CATEGORIES_SUCCESS,
+    payload: categories,
+  };
+}
+
+export function getCategories() {
+  return async function (dispatch) {
+    let url = " http://localhost:3001/categories";
+
+    const response = await fetch(url);
+    const result = Response.json();
+    return dispatch(getCategoriesSuccess(result));
+  };
+}
