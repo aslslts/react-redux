c7bbe1b283e724960b52b2628463321f5c443510
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-07 00:26:40 +0300

    Routing added

---
+++ northwind-redux/src/components/cart/CartDetail.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as cartActions from "../../redux/actions/cartActions";
+
+class CartDetail extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      removeFromCart: bindActionCreators(cartActions.removeFromCart),
+    },
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    cart: state.cartReducer,
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CartDetail);
