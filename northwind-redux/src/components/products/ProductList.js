8c27c86f64a3332be31d6462bb03bdbf6ca8c13b
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-06 23:04:06 +0300

    Adding to cart- React

--- northwind-redux/src/components/products/ProductList.js
+++ northwind-redux/src/components/products/ProductList.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Badge } from "reactstrap";
+import { Badge, Button } from "reactstrap";
 import { bindActionCreators } from "redux";
 import * as productActions from "../../redux/actions/productActions";
+import * as cartActions from "../../redux/actions/cartActions";
 import { Table } from "reactstrap";
+import alertify from "alertifyjs";
 class ProductList extends Component {
   componentDidMount() {
     this.props.actions.getProducts();
   }
+  addToCart = (product) => {
+    this.props.actions.addToCart({ quantity: 1, product });
+    alertify.success(product.productName + " added to cart");
+  };
   render() {
     return (
       <div>
@@ -25,6 +31,7 @@ class ProductList extends Component {
               <th>Unit Price</th>
               <th>Quantity Per Unit</th>
               <th>Units In Stock</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
@@ -35,6 +42,14 @@ class ProductList extends Component {
                 <td>{product.unitPrice}</td>
                 <td>{product.quantityPerUnit}</td>
                 <td>{product.unitsInStock}</td>
+                <td>
+                  <Button
+                    color="success"
+                    onClick={() => this.addToCart(product)}
+                  >
+                    Add
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
@@ -53,6 +68,7 @@ function mapDispatchToProps(dispatch) {
   return {
     actions: {
       getProducts: bindActionCreators(productActions.getProducts, dispatch),
+      addToCart: bindActionCreators(cartActions.addToCart, dispatch),
     },
   };
 }
