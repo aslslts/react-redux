748b12f7c7d998ded64578a10ca7b0684fc02c9b
Aslı Sulutaş <cemree.1402@gmail.com>
2022-12-08 02:19:24 +0300

    handleError added

--- northwind-redux/src/redux/actions/productActions.js
+++ northwind-redux/src/redux/actions/productActions.js
@@ -6,6 +6,41 @@ export function getProductsSuccess(products) {
     payload: products,
   };
 }
+export function createProductSuccess(product) {
+  return {
+    type: actionTypes.CREATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+export function updateProductSuccess(product) {
+  return {
+    type: actionTypes.UPDATE_PRODUCT_SUCCESS,
+    payload: product,
+  };
+}
+
+export function saveProductApi(product) {
+  return fetch("http://localhost:3001/products/" + (product.id || ""), {
+    method: product.id ? "PUT" : "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(product),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+export function saveProduct(product) {
+  return function (dispatch) {
+    return saveProductApi(product)
+      .then((savedProduct) => {
+        product.id
+          ? dispatch(updateProductSuccess(savedProduct))
+          : dispatch(createProductSuccess(savedProduct));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
 
 export function getProducts(categoryId) {
   return function (dispatch) {
@@ -18,4 +53,17 @@ export function getProducts(categoryId) {
       .then((response) => response.json())
       .then((result) => dispatch(getProductsSuccess(result)));
   };
+}
+
+export async function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+  const error = await response.text();
+  throw new Error(error);
+}
+
+export function handleError(error) {
+  console.error("Bir hata oluştu");
+  throw error;
 }
